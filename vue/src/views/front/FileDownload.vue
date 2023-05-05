90af755dd9ac3a83132eb8251f522edd585f5b4e
Jason <1402083402@qq.com>
2023-05-06 00:56:26 +0800

    前台下载完善

--- vue/src/views/front/FileDownload.vue
+++ vue/src/views/front/FileDownload.vue
@@ -6,21 +6,6 @@
       <el-button type="warning" @click="reset">重置</el-button>
     </div>
     <div style="margin: 10px 0">
-      <el-upload action="http://localhost:9090/file/upload" :show-file-list="false"
-                 :on-success="handleFileUploadSuccess" style="display: inline-block">
-        <el-button type="primary" class="ml-5">上传文件 <i class="el-icon-top"></i></el-button>
-      </el-upload>
-      <el-popconfirm
-          class="ml-5"
-          confirm-button-text='确定'
-          cancel-button-text='取消'
-          icon="el-icon-info"
-          icon-color="red"
-          title="您确定批量删除这些数据吗？"
-          @confirm="delBatch"
-      >
-        <el-button type="danger" slot="reference">批量删除 <i class="el-icon-remove-outline"></i></el-button>
-      </el-popconfirm>
 
     </div>
     <el-table :data="tableData" border stripe :header-cell-class-name="'headerBg'"
@@ -37,21 +22,7 @@
         </template>
       </el-table-column>
 
-      <el-table-column label="操作" width="200" align="center">
-        <template slot-scope="scope">
-          <el-popconfirm
-              class="ml-5"
-              confirm-button-text='确定'
-              cancel-button-text='我再想想'
-              icon="el-icon-info"
-              icon-color="red"
-              title="您确定删除吗？"
-              @confirm="del(scope.row.id)"
-          >
-            <el-button type="danger" slot="reference">删除 <i class="el-icon-remove-outline"></i></el-button>
-          </el-popconfirm>
-        </template>
-      </el-table-column>
+
     </el-table>
 
     <div style="padding: 10px 0">
@@ -107,30 +78,9 @@ export default {
         }
       })
     },
-    del(id) {
-      this.request.delete("/file/" + id).then(res => {
-        if (res.code === '200') {
-          this.$message.success("删除成功")
-          this.load()
-        } else {
-          this.$message.error("删除失败")
-        }
-      })
-    },
     handleSelectionChange(val) {
       console.log(val)
       this.multipleSelection = val
-    },
-    delBatch() {
-      let ids = this.multipleSelection.map(v => v.id)  // [{}, {}, {}] => [1,2,3]
-      this.request.post("/file/del/batch", ids).then(res => {
-        if (res.code === '200') {
-          this.$message.success("批量删除成功")
-          this.load()
-        } else {
-          this.$message.error("批量删除失败")
-        }
-      })
     },
     reset() {
       this.name = ""
